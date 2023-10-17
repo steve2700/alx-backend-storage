410a52d5a404a901dcf9e2949b1fd9b77b002dab
steve2700 <stevnyaru@github.com>
2023-10-17 04:50:47 -0700

    using autopep8 to remove whitespace is a great deal

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env/python3
-"""Write a Python function that returns the list 
-   of school having a specific topic:
+"""Write a Python function that returns the list
+of school having a specific topic
 
   Prototype: def schools_by_topic(mongo_collection, topic):
   mongo_collection will be the pymongo collection object
