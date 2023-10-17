de2dde72ac7553fa7ed9168204ead41e9538f18f
steve2700 <stevnyaru@github.com>
2023-10-17 05:32:25 -0700

    automate with autopep 8

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env/python3
-"""Write a Python function that returns the list
-of school having a specific topic
+#!/usr/bin/env python3
+"""Write a Python function that returns the list of school
+having a specific topic:
 
-  Prototype: def schools_by_topic(mongo_collection, topic):
-  mongo_collection will be the pymongo collection object
-  topic (string) will be topic searched
+Prototype: def schools_by_topic(mongo_collection, topic):
+mongo_collection will be the pymongo collection object
+topic (string) will be topic searched
 """
 
 
@@ -12,5 +12,5 @@ import pymongo
 
 
 def schools_by_topic(mongo_collection, topic):
-    """returns the list of school having specific topic"""
-    return mongo_collection.find({"topic": topic})
+    """returns the list of school having a specific topic"""
+    return mongo_collection.find({"topics": topic})
